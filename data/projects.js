949036bd19d3725f0d8359d0c9533c85132ea734
Kofi Asante <kofiasante2001@yahoo.com>
2025-03-24 15:43:19 -0400

    trying something

---
+++ data/projects.js
@@ -0,0 +1,10 @@
+export const githubProjects = [
+  {
+    title: "GameDay Schedule",
+    description: "An NBA games notifiation system",
+    url: "https://github.com/akadarkoh/gameday-schedule",
+    tags: ["React", "API"],
+    stars: 15
+  }
+  // Add more projects
+];
